44b48eb79d0ed49f98400aba39bb41ea021510e3
jos84mar20 <josemarulanda64@gmail.com>
2022-03-21 14:00:52 -0500

    ultimo

---
+++ app-1.ts
@@ -0,0 +1,8 @@
+
+
+//const incrementadorAction:Action={
+//    type:'INCREMENTAR'
+//};
+
+// Usar el reducer
+
---
+++ app-2.ts
@@ -0,0 +1,2 @@
+import { decrementadorAction, dividirAction, incrementadorAction, multiplicarAction, resetAction } from "./contador/contador.actions";
+import { Action } from "./ngrx-fake/ngrx";
